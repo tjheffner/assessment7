e5a31a7e1785e496edb43290fa6d66932ecfae1d
Tanner Heffner <tannerjheffner@gmail.com>
2015-04-24 09:58:55 -0700

    take out center alignment on all pages but home, add buttons to signin, haven't added hiding yet

--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,19 +1,19 @@
 studentsRoster.factory('StudentsFactory', function StudentsFactory() {
 
   var factory = {};
-  factory.students = [{Jesse Pinkman, false},
-                      {Walter White, false},
-                      {Slippin Jimmy, false},
-                      {Hank Schrader, false},
-                      {Mike Ehrmantraut, false},
-                      {Gustavo Fring, false},
-                      {Badger, false},
-                      {Steven Gomez, false},
-                      {Mr. Heisenberg, false},
-                      {Saul Goodman, false},
-                      {Chuck McGill, false},
-                      {Howard Hamlin, false},
-                      {Kim Wexler, false},
+  factory.students = [{name: 'Jesse Pinkman', here: false},
+                      {name: 'Walter White', here: false},
+                      {name: 'Slippin Jimmy', here: false},
+                      {name: 'Hank Schrader', here: false},
+                      {name: 'Mike Ehrmantraut', here: false},
+                      {name: 'Gustavo Fring', here: false},
+                      {name: 'Badger', here: false},
+                      {name: 'Steven Gomez', here: false},
+                      {name: 'Mr. Heisenberg', here: false},
+                      {name: 'Saul Goodman', here: false},
+                      {name: 'Chuck McGill', here: false},
+                      {name: 'Howard Hamlin', here: false},
+                      {name: 'Kim Wexler', here: false}
                      ];
 
   return factory;
